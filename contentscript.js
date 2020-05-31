14e83d522dbf5542e4863bd5c86ecbfeeebe5fca
Viet Dao <vietdao204@gmail.com>
2020-05-31 16:27:03 -0400

    accounting.js and money.js basic setup

--- contentscript.js
+++ contentscript.js
@@ -12,12 +12,22 @@ if (document.body != null) {
 	// if hover over an element containing $
 	$(":contains('$'):not(:has(:contains('$')))").hover(function() {
 		// grab the dollar amount and calculate
-		console.log($(this).text());
-		console.log('Attempting to display box now..');
+		var amountUSD = accounting.unformat($(this).text()); // USD default for now
+		fx.base = "USD";
+		fx.rates = {
+			"CAD" : 1.4, // eg. 1 USD === 0.745101 EUR
+			"VND" : 23281.5,
+			"USD" : 1,        // always include the base rate (1:1)
+		}
+		var amountCAD = fx.convert(amountUSD, {from: 'USD', to: 'CAD'});
+		var amountVND = fx.convert(amountUSD, {from: 'USD', to: 'VND'});
+		console.log('Attempting to display box now..' + amountUSD + "USD");
+		console.log('Attempting to display box now..' + amountCAD + "CAD");
+		console.log('Attempting to display box now..' + amountVND + "VND");
 		// insert calculated amount to exchange box
 		// display box at mouse pointer
 		$("#xchangeBox").css( "display", "block" );
   		}, function() {
   			$("#xchangeBox").css( "display", "none" )
-  		});
+  		});		
 }
