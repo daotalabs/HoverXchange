45cc046d4e573d789de10477296947b1b49632a5
Viet Dao <vietdao204@gmail.com>
2020-06-07 17:30:24 -0400

    get rates from storage in contentscript

--- background.js
+++ background.js
@@ -2,12 +2,21 @@ chrome.runtime.onInstalled.addListener(function() {
     console.log("xchange_xtension installation completed.");
 });
 
+chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    chrome.declarativeContent.onPageChanged.addRules([{
+    	conditions: [new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: {hostContains: "."},
+        })],
+        actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+});
+
+/*
+	Listen for tab updates, get and update exchange rates in Chrome sync storage. 
+*/
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-	// If local/sync storage has current rates, skip API calls
-	console.log('tab updated: #' + tabId);
 	if (changeInfo.status == 'complete' && tab.active) {
 		var ratesData = 'sample';
-		console.log('checking storage..');
 		chrome.storage.sync.get(null, function(items) {
 		    var allKeys = Object.keys(items);
 		    console.log('current storage: ' + allKeys);
@@ -21,28 +30,14 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 						base: 'USD'
 					}, 
 					function(response) {
-						ratesData = JSON.stringify(response);
 		    			console.log("1 US Dollar equals " + response.rates.GBP + " British Pounds");
 		    			chrome.storage.sync.set({'current_rates': response}, function() {
-							console.log('Saving current rates..' + ratesData);
+							console.log('Saving current rates..' + JSON.stringify(response));
 						})
 					});
 			} else {
-				console.log('Value currently is ' + value.current_rates.toString());
+				console.log('Value currently is ' + JSON.stringify(value));
 			}
 	    });
 	}
-	// API calls to get exchange rates
-
-	// Save rates to storage
-	
 })
-
-chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-    	conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostContains: "."},
-        })],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-});
--- contentscript.js
+++ contentscript.js
@@ -11,24 +11,48 @@ $(document).mousemove(function(e) {
 	CAD: domain .ca, url has /ca/
 	USD: default
 */
-var baseCurrency = 'USD'; // this will be determined on domains, etc.
+var websiteCurrency = 'USD'; // this will be determined on domains, etc.
 var xCurrencies = ['CAD', 'VND'] // this will be choosen on popup.html or by default
 
-fx.base = baseCurrency;
-// get rates from storage
-fx.rates = {
-	"CAD" : 1.4, 
-	"VND" : 23281.5,
-	"USD" : 1,        // always include the base rate (1:1)
+getExchangeRates();
+
+/*
+	Get exchange rates from storage API and display currency box.
+*/
+function getExchangeRates() {
+	chrome.storage.sync.get(['current_rates'], function(ratesData) {
+    	console.log('contentscript: Value currently is ' + JSON.stringify(ratesData));
+    	fx.base = ratesData.current_rates.base;
+    	console.log('contentscript: base currently is ' + fx.base);
+    	fx.rates = ratesData.current_rates.rates;
+    	console.log('contentscript: rates currently is ' + JSON.stringify(fx.rates));
+    	createCurrencyBox();
+	});
 }
 
-createCurrencyBox();
-
+/*
+	Display currency box on hover over a currency amount.
+*/
 function createCurrencyBox() {
 	if (document.body == null) {
 		return;
 	}
-	if (fx.base == 'USD') {
+	if (websiteCurrency == 'USD') {
+		/* CURRENCY SEARCH:
+		if page is USD:
+			if innermost element has $..
+			if innermost element has only number + immediate outer element has only $
+				then get amount
+		similarly,
+		if page is CAD:
+			if innermost element has $../C$../CAD../CAD$..
+			if innermost element has only number + immediate outer element has only $/C$/CAD/CAD$
+				then get amount
+		if page is VND:
+			if innermost element has ₫../VND../..₫
+			if innermost element has only number + immediate outer element has only ₫/VND
+				then get amount
+		*/
 		$(":contains('$'):not(:has(:contains('$')))").hover(function() { // HACK: if hover over an element containing $
 				// grab the dollar amount and calculate
 				var amountUSD = accounting.unformat($(this).text());
@@ -59,65 +83,10 @@ function createCurrencyBox() {
 	  			$('#xchangeBox').remove();
 	  		});	
 	}
-	if (fx.base == 'CAD') {
+	if (websiteCurrency == 'CAD') {
 
 	}	
-	if (fx.base == 'VND') {
+	if (websiteCurrency == 'VND') {
 
 	}
 }
-// if (document.body != null) {
-	// if element hovered over is a money amount, make text box visible at mouse pointer
-
-	/* CURRENCY SEARCH:
-	if page is USD:
-		if innermost element has $..
-		if innermost element has only number + immediate outer element has only $
-			then get amount
-	similarly,
-	if page is CAD:
-		if innermost element has $../C$../CAD../CAD$..
-		if innermost element has only number + immediate outer element has only $/C$/CAD/CAD$
-			then get amount
-	if page is VND:
-		if innermost element has ₫../VND../..₫
-		if innermost element has only number + immediate outer element has only ₫/VND
-			then get amount
-	*/
-	// if (fx.base == 'USD') {
-	// 	$(":contains('$'):not(:has(:contains('$')))").hover(function() { // HACK: if hover over an element containing $
-	// 			// grab the dollar amount and calculate
-	// 			var amountUSD = accounting.unformat($(this).text());
-	// 			var amountCAD = fx.convert(amountUSD, {from: 'USD', to: 'CAD'});
-	// 			var amountVND = fx.convert(amountUSD, {from: 'USD', to: 'VND'});
-
-	// 			// create xchange box element
-	// 			var exchangeBoxEl = document.createElement('div');
-	// 			var exchangeBoxStr = "<div id='xchangeBox'>" +
-	// 									"<span id='currency1' class='currency'>" +
-	// 										// insert calculated amount to exchange box
-	// 										accounting.formatMoney(amountCAD,[symbol = "C$"],[precision = 2],[thousand = ","],[decimal = "."],[format = "%s%v"]) +
-	// 									"</span>" +
-	// 									"<br>" +
-	// 									"<span id='currency2' class='currency'>" + 
-	// 										accounting.formatMoney(amountVND,[symbol = "₫"],[precision = 0],[thousand = ","],[decimal = "."],[format = "%s%v"]) +
-	// 									"</span>" +
-	// 								"</div>";	
-	// 			exchangeBoxEl.innerHTML = exchangeBoxStr;
-	// 			// add new invisible text box at the end of page
-	// 			document.body.appendChild(exchangeBoxEl.firstChild);
-	// 			// display box at mouse pointer
-	// 			$("#xchangeBox").css('top', mouseY + 'px');
-	// 			$("#xchangeBox").css('left', mouseX + 'px');
-	// 			$("#xchangeBox").css('display', 'block');
-	//   		}, function() {
-	//   			$('#xchangeBox').remove();
-	//   		});	
-	// }
-	// if (fx.base == 'CAD') {
-
-	// }	
-	// if (fx.base == 'VND') {
-
-	// }
-// }
