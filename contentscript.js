f179b6651104c4930b7773751abf6fc0812f76dc
Viet Dao <vietdao204@gmail.com>
2020-10-10 20:19:26 -0400

    refactor xchange display

--- contentscript.js
+++ contentscript.js
@@ -1,3 +1,18 @@
+var VND = 'VND';
+var USD = 'USD';
+var CAD = 'CAD';
+var EUR = 'EUR';
+var websiteCurrency = 'USD';
+var fxCurrencies = {};
+var displayCurrencies = [];
+
+/*
+	Regexes for currency search. https://regex101.com for help.
+*/
+var dollarOnlyRegex = /^[^0-9]*\$-?\s?(?:\d+|\d{1,3}(?:,\d{3})+)(?:(\.|,)\d+)?[^0-9]*$/i;
+var dongPreOnlyRegex = /^[^0-9]*(?:\₫|VND|VNĐ)\s?(?:\d+|\d{1,3}(?:.\d{3})+)(?:(\.|,)\d+)?[^0-9]*$/i;
+var dongSubOnlyRegex = /^[^0-9]*(?:\d+|\d{1,3}(?:.\d{3})+)(?:(\.|,)\d+)?\s?(?:\₫|VND|VNĐ)[^0-9]*$/i;
+
 /*
 	Get mouse pointer position to show xchangeBox.
 */
@@ -8,10 +23,6 @@ $(document).mousemove(function(e) {
 	mouseY = e.pageY + 15;
 });
 
-var websiteCurrency = 'USD';
-var fxCurrencies = {};
-var displayCurrencies = [];
-
 /*
 	Check if extension is enabled before running anything.
 */
@@ -21,20 +32,23 @@ chrome.storage.sync.get('xchangeXtensionOptions', function(value) {
 		return;
 	}
 	if (value.xchangeXtensionOptions.enabled) {
-		getDisplayCurrencies(getWebsiteCurrency);
+		getDisplayCurrencies();
+		getWebsiteCurrency(getExchangeRates);
 	}
 })
 
 /*
 	Get currencies from sync storage.
 */
-function getDisplayCurrencies(callback) {
+function getDisplayCurrencies() {
 	chrome.storage.sync.get('xchangeXtensionOptions', function(value) {
 		if (value.xchangeXtensionOptions == null || Object.keys(value.xchangeXtensionOptions).length == 0) {
 			console.error('Missing sync settings, stopping..');
 		} else {
 			displayCurrencies = value.xchangeXtensionOptions.displayCurrencies;
-			callback();
+			displayCurrencies.forEach(function (displayCurrency, index, array) {
+				fxCurrencies[displayCurrency] = '0';
+			});
 		}
 	})
 }
@@ -44,20 +58,15 @@ function getDisplayCurrencies(callback) {
 	CAD: domain .ca, url has /ca/
 	USD: default
 */
-function getWebsiteCurrency() {
+function getWebsiteCurrency(callback) {
 	var currentTabUrl = document.location.href;
 	// console.log('currentTabUrl: ' + currentTabUrl);
 	if (currentTabUrl.includes('.ca') || currentTabUrl.includes('.ca/') || currentTabUrl.includes('/ca/')) {
-		websiteCurrency = 'CAD';
+		websiteCurrency = CAD;
 	} else if (currentTabUrl.includes('.vn') || currentTabUrl.includes('.vn/') || currentTabUrl.includes('/vn/')) {
-		websiteCurrency = 'VND';
+		websiteCurrency = VND;
 	}
-	displayCurrencies.forEach(function (displayCurrency, index, array) {
-		if (websiteCurrency.localeCompare(displayCurrency) != 0) {
-			fxCurrencies[displayCurrency] = '0';
-		}
-	});
-	getExchangeRates();
+	callback();
 }
 
 /*
@@ -67,8 +76,8 @@ function getExchangeRates() {
 	chrome.storage.local.get('xchangeXtensionRates', function(value) {
     fx.base = value.xchangeXtensionRates.currentRates.base;
   	fx.rates = value.xchangeXtensionRates.currentRates.rates;
-  	createCurrencyBox();
 	});
+	createCurrencyBox();
 }
 
 /*
@@ -78,78 +87,53 @@ function createCurrencyBox() {
 	if (document.body == null) {
 		return;
 	}
-	// regexes for currency search
-	var dollarOnlyRegex = /^[^0-9]*\$-?\s?(?:\d+|\d{1,3}(?:,\d{3})+)(?:(\.|,)\d+)?[^0-9]*$/i;
-	var dongPreOnlyRegex = /^[^0-9]*\₫\s?(?:\d+|\d{1,3}(?:.\d{3})+)(?:(\.|,)\d+)?[^0-9]*$/i;
-	var dongSubOnlyRegex = /^[^0-9]*(?:\d+|\d{1,3}(?:.\d{3})+)(?:(\.|,)\d+)?\s?\₫[^0-9]*$/i;
-	/* CURRENCY SEARCH:
-		if page is VND:
-			[done] if innermost element has ₫(number)/(number)₫ surrounded by non-integer characters
-			[later] if innermost element has (number)K(space) surrounded by non-integer characters
-			[later] if highlighted
-			[later] if innermost element has only number + immediate outer element has only ₫/VND
-				then get amount
-		if page is USD:
-			[done] if innermost element has $(number) or $(number) surrounded by non-integer characters
-			[later] if highlighted
-			[later-maybe] if innermost element has only number + immediate outer element has only $
-				then get amount
-		similarly,
-		if page is CAD:
-			[done] if innermost element has $(number) or $(number) surrounded by non-integer characters
-			[later] if highlighted
-			[later] if innermost element has CAD(number) surrounded by non-integer characters
-			[later-maybe] if innermost element has only number + immediate outer element has only $/C$/CAD/CAD$
-				then get amount
-	*/
-	if (websiteCurrency == 'VND') {
-		$(':contains("₫"):not(:has(:contains("₫")))').filter(function() {
-			return dongPreOnlyRegex.test($(this).text()) || dongSubOnlyRegex.test($(this).text());
-		}).hover(function() {
-			var amount = accounting.unformat($(this).text(), ',');
-			// console.log(amount + '; regex tested: ' + $(this).text());
-			getFxAmounts(amount, displayCurrencyBox)
-		}, function() {
-			$('#xchangeBox').remove();
-		});
-	} else {
-		$(':contains("$"):not(:has(:contains("$")))').filter(function() {
-			return dollarOnlyRegex.test($(this).text());
-		}).hover(function() {
-			var amount = accounting.unformat($(this).text());
-			//console.log(amount + '; regex tested: ' + $(this).text());
-			getFxAmounts(amount, displayCurrencyBox)
-		}, function() {
-			$('#xchangeBox').remove();
-		});
-	}
+	$(':contains("₫"):not(:has(:contains("₫"))), :contains("VND"):not(:has(:contains("VND"))), :contains("VNĐ"):not(:has(:contains("VNĐ")))').filter(function() {
+		return dongPreOnlyRegex.test($(this).text()) || dongSubOnlyRegex.test($(this).text());
+	}).hover(function() {
+		var amount = accounting.unformat($(this).text(), ',');
+		// console.log(amount + '; regex tested: ' + $(this).text());
+		getFxAmounts(amount, VND, displayCurrencyBox)
+	}, function() {
+		$('#xchangeBox').remove();
+	});
+
+	$(':contains("$"):not(:has(:contains("$")))').filter(function() {
+		return dollarOnlyRegex.test($(this).text());
+	}).hover(function() {
+		var amount = accounting.unformat($(this).text());
+		console.log(amount + '; regex tested: ' + $(this).text());
+		getFxAmounts(amount, websiteCurrency, displayCurrencyBox)
+	}, function() {
+		$('#xchangeBox').remove();
+	});
 }
 
 /*
 	Convert default website currency to other currencies.
 */
-function getFxAmounts(amount, callback){
+function getFxAmounts(amount, baseCurrency, callback){
 	Object.keys(fxCurrencies).forEach(function(displayCurrency) {
-		fxCurrencies[displayCurrency] = formatFx(displayCurrency, fx.convert(amount, {from: websiteCurrency, to: displayCurrency}));
+		fxCurrencies[displayCurrency] = formatFx(displayCurrency, fx.convert(amount, {from: baseCurrency, to: displayCurrency}));
 		// console.log(key, fxCurrencies[key]);
 	});
-	callback(fxCurrencies);
+	callback(fxCurrencies, baseCurrency);
 }
 
 /*
 	Create and display currency box.
 */
-function displayCurrencyBox(fxCurrencies) {
+function displayCurrencyBox(fxCurrencies, baseCurrency) {
 	var exchangeBoxEl = document.createElement('div');
 	var exchangeBoxStr = '<div id="xchangeBox">';
 	Object.keys(fxCurrencies).forEach(function(key) {
-		exchangeBoxStr = exchangeBoxStr + '<span class="currency">' +
-										 fxCurrencies[key] + '</span>' +
-							  		 '<br>';
+		if (key.localeCompare(baseCurrency) != 0) {
+			exchangeBoxStr = exchangeBoxStr + '<span class="currency">' +
+											 fxCurrencies[key] + '</span>' +
+								  		 '<br>';
+		}
 	});
 	exchangeBoxStr = exchangeBoxStr + '</div>';
 	exchangeBoxEl.innerHTML = exchangeBoxStr;
-	// add new invisible box at the end of page
 	document.body.appendChild(exchangeBoxEl.firstChild);
 	// display box slightly below mouse pointer
 	$('#xchangeBox').css('top', mouseY + 'px');
